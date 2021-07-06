4195c3b71f0ebb84290a22e70287490d6918fdaf
kmfriedt <kfriedt@gmail.com>
2021-07-06 11:42:54 -0600

    Implemented hello world

---
+++ hello-rust/target/debug/deps/hello_rust-d1d4d0ee94f2ac00.d
@@ -0,0 +1,5 @@
+/home/kfriedt/code/rust-code/rust-by-example/hello-rust/target/debug/deps/hello_rust-d1d4d0ee94f2ac00: src/main.rs
+
+/home/kfriedt/code/rust-code/rust-by-example/hello-rust/target/debug/deps/hello_rust-d1d4d0ee94f2ac00.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/kfriedt/code/rust-code/rust-by-example/hello-rust/target/debug/hello-rust: /home/kfriedt/code/rust-code/rust-by-example/hello-rust/src/main.rs
